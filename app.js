4c35ea88756ed51e6135cc7515321d05c5ade43e
Zoubeiri <housna.zoubeiri@gmail.com>
2021-09-24 12:16:31 +0200

    Adding MongoDB password

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://hzoubeiri:<password>@cluster0.7e2xu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://hzoubeiri:IrlasNahouza2016@cluster0.7e2xu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
- });
+  app.use(express.json());
+  app.use(express.urlencoded({
+    extended:true
+  }));
 
 module.exports = app;
