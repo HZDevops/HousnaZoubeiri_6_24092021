6b3f42a7ca95c8ba4e185aae17bdfcf22e432311
fatb38 <arnaud.groby@gmail.com>
2021-09-26 21:51:03 +0200

    fixing gitignore + mongoose connect method

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://hzoubeiri:IrlasNahouza2016@cluster0.7e2xu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect(
+  'mongodb+srv://hzoubeiri:IrlasNahouza2016@cluster0.7e2xu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err) => {
+    if (err) {
+     return console.log('Connexion à MongoDB échouée !', err)
+    }
+    console.log('Connexion à MongoDB réussie !')
+  }
+);
 
-  app.use(express.json());
-  app.use(express.urlencoded({
-    extended:true
-  }));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 module.exports = app;
