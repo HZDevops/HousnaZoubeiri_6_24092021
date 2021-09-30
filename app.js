2c67b27336e8754f627b44f0f83fbd32d0b15a80
Zoubeiri <housna.zoubeiri@gmail.com>
2021-09-30 10:17:28 +0200

    adding dotenv

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
 mongoose.connect(
-  'mongodb+srv://hzoubeiri:IrlasNahouza2016@cluster0.7e2xu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  `mongodb+srv://hzoubeiri:${DB_PASSWORD}@cluster0.7e2xu.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
