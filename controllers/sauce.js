92c2427a5eecd976c0f943104b3db66a951446b5
Arnaud <arno.tgh@gmail.com>
2021-10-06 19:15:26 +0200

    fixing like / dislike function

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,11 +1,7 @@
-//Import Sauce schema from models
 const ModelsSauce = require('../models/sauce');
-
-//Import fs package for managing files
 const fs = require('fs');
 
-//Create a new sauce
-function createSauce(req, res, next) {
+function createSauce (req, res) {
   const sauceObject = JSON.parse(req.body.sauce);
   const sauce = new ModelsSauce({
     ...sauceObject,
@@ -15,7 +11,7 @@ function createSauce(req, res, next) {
     likes: 0,
     dislikes: 0,
     usersLiked: [],
-    usersDisliked: [],
+    usersDisliked: []
   });
   sauce
     .save()
@@ -23,15 +19,14 @@ function createSauce(req, res, next) {
     .catch((error) => res.status(400).json({ error }));
 }
 
-//Modify a sauce
-function modifySauce(req, res, next) {
+function modifySauce (req, res) {
   const sauceObject = req.file
     ? {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${
-          req.file.filename
-        }`,
-      }
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${
+        req.file.filename
+      }`
+    }
     : { ...req.body };
   ModelsSauce.updateOne(
     { _id: req.params.id },
@@ -41,8 +36,7 @@ function modifySauce(req, res, next) {
     .catch((error) => res.status(400).json({ error }));
 }
 
-//Delete a sauce
-function deleteSauce(req, res, next) {
+function deleteSauce (req, res) {
   ModelsSauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       const filename = sauce.imageUrl.split('/images/')[1];
@@ -55,142 +49,66 @@ function deleteSauce(req, res, next) {
     .catch((error) => res.status(500).json({ error }));
 }
 
-//Get one sauce by id
-function getOneSauce(req, res, next) {
+function getOneSauce (req, res) {
   ModelsSauce.findOne({ _id: req.params.id })
     .then((sauce) => res.status(200).json(sauce))
     .catch((error) => res.status(404).json({ error }));
 }
 
-//Get all sauces
-function getAllSauce(req, res, next) {
+function getAllSauce (req, res) {
   ModelsSauce.find()
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(400).json({ error }));
 }
 
-//Like or Dislike a sauce
-function likeDislike(req, res, next) {
+function likeOrDislikeSauce (req, res) {
   let like = req.body.like;
   let userId = req.body.userId;
   let sauceId = req.params.id;
 
   if (like === 1) {
     ModelsSauce.updateOne(
-      {
-        _id: sauceId,
-      },
-      {
-        $push: {
-          usersLiked: userId,
-        },
-        $inc: {
-          likes: +1,
-        },
-      }
+      { _id: sauceId },
+      { $push: { usersLiked: userId }, $inc: { likes: +1 } }
     )
-      .then(() =>
-        res.status(200).json({
-          message: 'Like added!',
-        })
-      )
-      .catch((error) =>
-        res.status(400).json({
-          error,
-        })
-      );
+      .then(() => res.status(200).json({ message: 'Like added!' }))
+      .catch(error => res.status(400).json({ error }));
   }
+
   if (like === -1) {
     ModelsSauce.updateOne(
-      {
-        _id: sauceId,
-      },
-      {
-        $push: {
-          usersDisliked: userId,
-        },
-        $inc: {
-          dislikes: +1,
-        },
-      }
+      { _id: sauceId },
+      { $push: { usersDisliked: userId }, $inc: { dislikes: +1 } }
     )
-      .then(() => {
-        res.status(200).json({
-          message: 'Dislike added !',
-        });
-      })
-      .catch((error) =>
-        res.status(400).json({
-          error,
-        })
-      );
+      .then(() => res.status(200).json({ message: 'Dislike added !' }))
+      .catch(error => res.status(400).json({ error }));
   }
 
   // Cancel a Like or Dislike
   if (like === 0) {
-    ModelsSauce.findOne({
-      _id: sauceId,
-    })
+    ModelsSauce.findOne({ _id: sauceId })
       .then((sauce) => {
-        //Cancel a Like
+        // Cancel a Like
         if (sauce.usersLiked.includes(userId)) {
-          Sauce.updateOne(
-            {
-              _id: sauceId,
-            },
-            {
-              $pull: {
-                usersLiked: userId,
-              },
-              $inc: {
-                likes: -1,
-              },
-            }
+          ModelsSauce.updateOne(
+            { _id: sauceId },
+            { $pull: { usersLiked: userId }, $inc: { likes: -1 } }
           )
-            .then(() =>
-              res.status(200).json({
-                message: 'Like removed !',
-              })
-            )
-            .catch((error) =>
-              res.status(400).json({
-                error,
-              })
-            );
+            .then(() => res.status(200).json({ message: 'Like removed !' }))
+            .catch(error => res.status(400).json({ error }));
         }
 
         //Cancel a Dislike
         if (sauce.usersDisliked.includes(userId)) {
           ModelsSauce.updateOne(
-            {
-              _id: sauceId,
-            },
-            {
-              $pull: {
-                usersDisliked: userId,
-              },
-              $inc: {
-                dislikes: -1,
-              },
-            }
+            { _id: sauceId },
+            { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 } }
           )
-            .then(() =>
-              res.status(200).json({
-                message: 'Dislike removed !',
-              })
-            )
-            .catch((error) =>
-              res.status(400).json({
-                error,
-              })
-            );
+            .then(() => res.status(200).json({ message: 'Dislike removed !' }))
+            .catch(error => res.status(400).json({ error }));
         }
       })
-      .catch((error) =>
-        res.status(404).json({
-          error,
-        })
-      );
+      .catch(error => res.status(404).json({ error }));
   }
 }
 
@@ -200,5 +118,5 @@ module.exports = {
   deleteSauce,
   getOneSauce,
   getAllSauce,
-  likeDislike,
+  likeOrDislikeSauce
 };
--- routes/sauce.js
+++ routes/sauce.js
@@ -12,7 +12,7 @@ const multer = require('../middleware/multer-config');
 //Add Sauce routes with authentication security and multer for the post and put routes
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.post('/:id/like', auth, sauceCtrl.likeDislike);
+router.post('/:id/like', auth, sauceCtrl.likeOrDislikeSauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauce);
